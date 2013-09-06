c3392c6d25d2c87e46fedb197f002a8dcced9bc0
xavier <xavier@www>
2013-09-05 21:16:21 -0800

    * no point in keeping these around, the Qt Model/View approach for a prop grid is working much better, thank you
    [git-p4: depot-paths = "//teardrop/mainline/": change = 112]

--- tools/QtPropertyGrid/QtPropertyGrid.h
+++ tools/QtPropertyGrid/QtPropertyGrid.h
@@ -8,8 +8,6 @@ is prohibited.
 #if !defined(TEARDROP_QTPROPERTYGRID_INCLUDED)
 #define TEARDROP_QTPROPERTYGRID_INCLUDED
 
-#if !defined(USE_QTTREEPROPERTYBROWSER)
-
 #include <QTreeView>
 
 namespace Teardrop
@@ -46,75 +44,4 @@ namespace Teardrop
 		};
 	}
 }
-#else
-#include "QtPropertyBrowser/qttreepropertybrowser.h"
-#include <list>
-
-class QtStringPropertyManager;
-class QtDoublePropertyManager;
-class QtIntPropertyManager;
-class QtBoolPropertyManager;
-class QtGroupPropertyManager;
-class QtLineEditFactory;
-class QtCheckBoxFactory;
-class QtDoubleSpinBoxFactory;
-
-namespace Teardrop
-{
-	namespace Reflection
-	{
-		class Object;
-		class PropertyDef;
-	}
-
-	namespace Tools
-	{
-		class PropertyHelper;
-		class FileChooserFactory;
-
-		class QtPropertyGrid 
-			: public QtTreePropertyBrowser
-		{
-			Q_OBJECT
-
-		public:
-			QtPropertyGrid(QWidget* parent);
-			~QtPropertyGrid();
-
-			// PropertyGrid implementation
-			void setObject(Reflection::Object* object, Reflection::Object* metadata = 0);
-
-		protected:
-			Reflection::Object* mObject;
-			Reflection::Object* mMetadata;
-
-			QtStringPropertyManager* mStringPropMgr;
-			QtStringPropertyManager* mPathPropMgr;
-			QtDoublePropertyManager* mDoublePropMgr;
-			QtIntPropertyManager* mIntPropMgr;
-			QtBoolPropertyManager* mBoolPropMgr;
-			QtGroupPropertyManager* mGroupPropMgr;
-
-			QtLineEditFactory* mLineEditFactory;
-			QtDoubleSpinBoxFactory* mDoubleSpinBoxFactory;
-			QtCheckBoxFactory* mCheckBoxFactory;
-			FileChooserFactory* mFileChooserFactory;
-
-			void dragEnterEvent(QDragEnterEvent* event);
-			void dragMoveEvent(QDragMoveEvent* event);
-			void dragLeaveEvent(QDragLeaveEvent* event);
-			void dropEvent(QDropEvent* event);
-
-		private:
-			void clearHelpers();
-			void addProperty(QtProperty* parent, Reflection::Object* obj, const Reflection::PropertyDef* propDef);
-
-			typedef std::list<PropertyHelper*> PropertyHelpers;
-			PropertyHelpers mHelpers;
-		
-		private slots:
-		};
-	}
-}
-#endif // USE_QTTREEPROPERTYBROWSER
 #endif // TEARDROP_QTPROPERTYGRID_INCLUDED
