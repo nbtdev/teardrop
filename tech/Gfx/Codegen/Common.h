9ddbf75852cde878f5ca52e7e9f841db7716d882
Xavier <xavier@netbattletech.com>
2018-10-14 00:51:49 -0700

    update copyright year

--- tech/Gfx/Codegen/Common.h
+++ tech/Gfx/Codegen/Common.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-Copyright (c) 2015 Teardrop Games
+Copyright (c) 2018 Teardrop Games
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
